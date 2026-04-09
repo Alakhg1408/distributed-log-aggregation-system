b59d607d8872c0540372bab756a3b68469296b4f
Alakh Gupta <alakhgupta@Alakhs-MacBook-Air.local>
2026-04-09 11:27:13 +0530

    Added server and client files

--- client.py
+++ client.py
@@ -1,11 +1,31 @@
 import socket
 import time
 import random
+from cryptography.fernet import Fernet
 
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 9999
 
+#encryption using Fernet (symmetric encryption)
+key = Fernet.generate_key()
+cipher = Fernet(key)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(2)
+
+#Send key once
+while True:
+    sock.sendto(b"key" + key, (SERVER_IP, SERVER_PORT))
+    print("Sending key")
+    try:
+        data, _ = sock.recvfrom(1024)
+        if data == b"OK":
+            print("Sending logs:")
+            break
+    except socket.timeout:
+        print("Retry")
+        time.sleep(1)
+
 #AF_INET->IPv4
 #SOCK_DGRAM->UDP
 
@@ -25,8 +45,9 @@ while True:
     message = random.choice(messages) #random message 
 
     log = f"{timestamp}|{machine}|{message}"
+    encrypted_log = cipher.encrypt(log.encode()) #encrypting the log
 
-    sock.sendto(log.encode(), (SERVER_IP, SERVER_PORT))
+    sock.sendto(encrypted_log, (SERVER_IP, SERVER_PORT))
     #log.encode()->convert string to bytes
 
     time.sleep(0.01)  #100 logs/sec total
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 from log import start_log_processor
+from cryptography.fernet import Fernet
 
 HOST = "0.0.0.0"
 PORT = 9999
@@ -10,17 +11,33 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((HOST, PORT))
 
 print(f"Server listening on port {PORT}...")
+cipher = None  #will store Fernet object
 
 log_queue = []
 max = 1000
 dropped = 0
+client_ciphers = {}
 
 #Start log processing thread
 start_log_processor(log_queue)
 
 while True:
     data, addr = sock.recvfrom(1024)
-    log = data.decode()
+    if data.startswith(b"key"):
+        key = data[3:]
+        cipher = Fernet(key)
+        client_ciphers[addr]=cipher
+        print("Received key from client")
+        sock.sendto(b"OK", addr)
+        continue
+    if addr not in client_ciphers:
+        continue
+    # Decrypt logs
+    cipher = client_ciphers[addr]
+    try:
+        log = cipher.decrypt(data).decode()
+    except:
+        print("Decryption failed")
 
     #Backpressure handling
     if len(log_queue) >= max:
